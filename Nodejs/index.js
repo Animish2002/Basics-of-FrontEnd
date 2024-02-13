d8837ab1d2d5b6fd9e3bb3e9be19120e15f82d26
Animish2002 <animishchopade123@gmail.com>
2024-02-13 23:17:12 +0530

    change in js

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,5 +1,21 @@
-const add = (a,b) =>{
-    return a + b;
-}
+// const add = (a,b) =>{
+//     return a + b;
+// }
 
-console.log(add(1,3));
+// console.log(add(1,3));
+
+// const str = () =>{
+//     console.log("Hayyy there!!!");
+// } 
+
+// str();
+
+const obj ={
+    value: 20,
+    myFunction: () =>{
+        console.log(this);
+    },
+
+};
+
+obj.myFunction();
